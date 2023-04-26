33a4c2cdcd71b7c7f866d14fae16d646e3e15c59
int0p <aaa@s-Mac-mini.local>
2023-04-27 08:36:19 +0900

    store

---
+++ src/store/list.js
@@ -0,0 +1,29 @@
+import {writable} from 'svelte/store';
+
+// repo에서 데이터 받아옴.
+const repoLists = JSON.parse(window.localStorage.getItem('lists')) || [];
+
+// 받아온데이터 내부에서 사용
+const _lists = writable();
+_lists.subscribe($lists => {
+    // _lists 스토어에 저장된 값이 바뀌면, 그 값을 변수 $lists로 받아 로컬스토리지에 'lists' key로 저장하겠다.
+    window.localStorage.setItem('lists',JSON.stringify($lists));
+})
+
+//custom store
+export const lists ={
+    subscribe: _lists.subscribe,//자동구독
+    add(payload){
+        const{title} = payload; //구조분해로 payload에서 title끄냄.
+        _lists.update(
+            $lists => {
+                $lists.push({
+                    id:'',
+                    title,
+                    card:[]
+                })
+                return $lists
+            }
+        )
+    }
+}
